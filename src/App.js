62ae6c8ba4527dd07ca82cdc9c0bcae59a5465bb
ali-consciousroad <ali_dindar@live.be>
2024-10-29 19:20:49 +0100

    Transition to a controlled component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = () => {
+    console.log("Form submitted!")
+  };
+
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <div className="Field">
             <label>Name:</label>
-            <input type="text" placeholder="Name" name="name" />
+            <input 
+              type="text" 
+              placeholder="Name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)} 
+            />
           </div>
           <button type="submit">Submit</button>
         </fieldset>
